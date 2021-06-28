69daccc5801d62268768f7c9de02bc82d0c4cda0
Homework <diman.marinin17@gmail.com>
2021-06-28 22:18:41 +0400

    v1

---
+++ Yellow/matrix.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Matrix
+{
+public:
+	Matrix(){
+		vector<int> t[0];
+		vector<vector<int> [0]> tmp[0];
+		m = tmp;
+	};
+	// Matrix(int num_rows, int num_cols){
+	// 	vector<int> t[num_cols];
+	// 	vector<t> tmp[num_rows];
+	// 	m = tmp;
+	// };
+
+
+
+	~Matrix();
+	
+private:
+	// vector<int> row;
+	vector<vector<int>> m;
+};
+
+int main(){
+
+	return 0;
+}
