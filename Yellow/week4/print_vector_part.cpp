36277cf338a0b636df8599eb89c3a77e773334e3
Homework <diman.marinin17@gmail.com>
2021-07-14 18:03:57 +0400

    v7.14

---
+++ Yellow/week4/print_vector_part.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+void PrintVectorPart(const vector<int>& numbers){
+	auto r = find_if(numbers.begin(), numbers.end(), [](const int& n){return n < 0;});
+	for (auto it = negative_it; it != numbers.begin(); ) {
+		cout << *(--it) << " ";
+	}
+}
+
+
+int main() {
+  PrintVectorPart({6, 1, 8, -5, 4});
+  cout << endl;
+  PrintVectorPart({-6, 1, 8, -5, 4});  // ничего не выведется
+  cout << endl;
+  PrintVectorPart({6, 1, 8, 5, 4});
+  cout << endl;
+  return 0;
+}
