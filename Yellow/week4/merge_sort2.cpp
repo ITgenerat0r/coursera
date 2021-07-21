13242e7958fe7f79876728cbe14a911d665bbd95
WorkMachine <ydalite02@gmail.com>
2021-07-21 14:16:24 +0400

    v7.21

--- Yellow/week4/merge_sort2.cpp
+++ Yellow/week4/merge_sort2.cpp
@@ -8,23 +8,29 @@ using namespace std;
 template <typename RandomIt>
 void MergeSort(RandomIt range_begin, RandomIt range_end){
 
+
 	if(range_begin == range_end || range_begin+1 == range_end){
 		return ;
 	}
 
-	vector<typename RandomIt::value_type> elements(range_begin, range_end);
-	
-	MergeSort(begin(elements), begin(elements)+elements.size()/3);
-	MergeSort(begin(elements)+elements.size()/3, begin(elements)+elements.size()/3*2);
-	MergeSort(begin(elements)+elements.size()/3*2, end(elements));
-	vector<int> back_inserter(elements.size()/3*2); 
-	
-	merge(begin(elements), begin(elements)+elements.size()/3,
-		 begin(elements)+elements.size()/3, begin(elements)+elements.size()/3*2, 
-		 begin(back_inserter));
+	int size = range_end - range_begin;
 
-	merge(begin(back_inserter), end(back_inserter), 
-		begin(elements)+elements.size()/3*2, end(elements), 
+	vector<typename RandomIt::value_type> elements(range_begin, range_end);
+
+	MergeSort(begin(elements), begin(elements)+size/3);
+	MergeSort(begin(elements)+size/3, begin(elements)+size/3*2);
+	MergeSort(begin(elements)+size/3*2, end(elements));
+	
+	vector<typename RandomIt::value_type> extra;
+
+	auto b = back_inserter(extra); 
+	
+	merge(begin(elements), begin(elements)+size/3,
+		 begin(elements)+size/3, begin(elements)+size/3*2, 
+		 b);
+
+	merge(begin(extra), end(extra), 
+		begin(elements)+size/3*2, end(elements), 
 		range_begin);
 }
 
@@ -44,3 +50,29 @@ int main() {
   cout << endl;
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+// vector<typename RandomIt::value_type> elements(range_begin, range_end);
+
+// 	MergeSort(begin(elements), begin(elements)+elements.size()/3);
+// 	MergeSort(begin(elements)+elements.size()/3, begin(elements)+elements.size()/3*2);
+// 	MergeSort(begin(elements)+elements.size()/3*2, end(elements));
+	
+// 	vector<typename RandomIt::value_type> back_inserter(begin(elements), begin(elements)+elements.size()/3*2); 
+	
+// 	merge(begin(elements), begin(elements)+elements.size()/3,
+// 		 begin(elements)+elements.size()/3, begin(elements)+elements.size()/3*2, 
+// 		 begin(back_inserter));
+
+// 	merge(begin(back_inserter), end(back_inserter), 
+// 		begin(elements)+elements.size()/3*2, end(elements), 
+// 		range_begin);
