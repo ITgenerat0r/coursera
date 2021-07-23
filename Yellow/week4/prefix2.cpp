ff3a139bcb36323db9814f03ad5850bcda3d2037
Homework <diman.marinin17@gmail.com>
2021-07-23 21:35:07 +0400

    v7.23

--- Yellow/week4/prefix2.cpp
+++ Yellow/week4/prefix2.cpp
@@ -14,61 +14,78 @@ using namespace std;
 
 
 
-bool testL(const string& s, const string& prefix){
-	cout << "testL\n";
-	for(int i = 0; i < prefix.size(); i++){
-			cout << " - " << prefix[i] << " " << s[i] << endl;
-			if(s[i] < prefix[i]) {
-				cout << "false" << endl;
-				return false;
-			}
-		}
-		cout << "true" << endl;
-		return true;
-}
-
-
-bool testU(const string& s, const string& prefix){
-	cout << "testU\n";
-	bool e = false;
-	for(int i = 0; i < prefix.size(); i++){
-			cout << " - " << prefix[i] << " " << s[i] << endl;
-			if(s[i] < prefix[i]) {
-				cout << "false" << endl;
-				return false;
-			} else if(s[i] > prefix[i]){
-				e = true;
-			}
-		}
-		cout << "true" << endl;
-		if (e){
-			return true;
-		}else{
+bool test(const string s, const string prefix){
+	cout << "\nTEST\n";
+	cout << s << " ? " << prefix << endl;
+	size_t	 dl = min(s.size(), prefix.size());
+	for(int i = 0; i < dl; i++){
+		cout << " - " << s[i] << " " << prefix[i] << endl;
+		if(s[i] > prefix[i]) {
+			cout << "false" << endl << endl;
 			return false;
+		} else if(s[i] < prefix[i]){
+			cout << "true" << endl << endl;
+			return true;
 		}
+	}
+	cout << "true" << endl << endl;
+	return true;
 }
 
 
 template <typename RandomIt>
 pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, const string& prefix){
-	RandomIt it_begin, it_end;
+	RandomIt it, tmp;
+	tmp = range_begin;
+    typename std::iterator_traits<RandomIt>::difference_type count, step;
+    count = std::distance(range_begin, range_end);
+ 	cout << "Count " << count << endl;
+    while (count > 0) {
+        it = range_begin; 
+        step = count / 2; 
+        std::advance(it, step);
+        if (test(*it, prefix)) {
+            range_begin = ++it; 
+            count -= step + 1; 
+            cout << "<" << count << endl;
+        } 
+        else 
+        {
+        	count = step;
+        	cout << ">=" << count << endl;
+        }
+    }
+    RandomIt first = range_begin;
+    // range_begin = tmp;
+    count = std::distance(range_begin, range_end);
 
-	it_begin = lower_bound(range_begin, range_end, prefix, testL);
+    cout << "->2\n";
+    cout << count << endl;
+    while (count > 0) {
+        it = range_begin; 
+        step = count / 2; 
+        std::advance(it, step);
+        if (!(test(prefix, *it))) {
+            range_begin = ++it;
+            count -= step + 1;
+        } 
+        else
+            count = step;
+    }
+    RandomIt last = range_begin;
 
 
-	it_end = lower_bound(range_begin, range_end, prefix, testU);
 
-	// it_end = find_if(range_begin, range_end, [prefix](string s){
-	// 	for(int i = 0; i <= prefix.size(); i++){
-	// 		if(prefix[i] != s[i]) {return false;}
-	// 	}
-	// 	return true;
-	// });
+    // RandomIt first, last;
+
+    // first = upper_bound(range_begin, range_end, prefix, test);
+    // cout << "2222222222222222222222222222222222222222222222222222222222\n";
+    // last = lower_bound(range_begin, range_end, prefix, test);
 
 
-	pair<RandomIt, RandomIt> r = make_pair(it_begin, it_end);
-
-	cout << "   Info " << *r.first << " " << *r.second << endl;
+    cout << "count " << distance(first, range_end) << " " << distance(last, range_end) << endl;
+    cout << "	INFO " << *first << " " << *last << endl;
+	pair<RandomIt, RandomIt> r = make_pair(first, last);	
 	return r;
 }
 
@@ -77,8 +94,9 @@ pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end
     
 
 int main() {
-  const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
+  const vector<string> sorted_strings = {"aaa", "minsk", "moscow", "motovilikha", "murmansk"};
   
+
   const auto mo_result =
       FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
   for (auto it = mo_result.first; it != mo_result.second; ++it) {
