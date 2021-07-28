33649d4e994b395f1e6b9d642d3036fd8a4be3e8
Homework <diman.marinin17@gmail.com>
2021-07-28 13:57:18 +0400

    v7.28

--- Yellow/week4/accumulate.cpp
+++ Yellow/week4/accumulate.cpp
@@ -1,188 +1,109 @@
 #include <iostream>
-#include <map>
 #include <string>
-#include <sstream>
+#include <vector>
 #include <numeric>
-
+#include <sstream>
+#include <chrono>
 
 using namespace std;
 
+pair<string, string> SplitTwoStrict(string s, string delimiter) {
+  const size_t pos = s.find(delimiter);
+  if (pos == s.npos) {
+    return {s, ""};
+  } else {
+    return {s.substr(0, pos), s.substr(pos + delimiter.length())};
+  }
+}
 
+pair<string, string> SplitTwo(string s, string delimiter) {
+  const auto [lhs, rhs] = SplitTwoStrict(s, delimiter);
+  return {lhs, rhs};
+}
 
+string ReadToken(string& s, string delimiter = " ") {
+  const auto [lhs, rhs] = SplitTwo(s, delimiter);
+  s = rhs;
+  return lhs;
+}
 
-struct Date
-{
-	int year;
-	int month;
-	int day;
+int ConvertToInt(string str) {
+  size_t pos;
+  const int result = stoi(string(str), &pos);
+  if (pos != str.length()) {
+    stringstream error;
+    error << "string " << str << " contains " << (str.length() - pos) << " trailing chars";
+    throw invalid_argument(error.str());
+  }
+  return result;
+}
+
+class Date {
+public:
+  static Date FromString(string str) {
+    const int year = ConvertToInt(ReadToken(str, "-"));
+    const int month = ConvertToInt(ReadToken(str, "-"));
+    const int day = ConvertToInt(str);
+    return {year, month, day};
+  }
+
+  // Weird legacy, can't wait for chrono::year_month_day
+  time_t AsTimestamp() const {
+    tm t;
+    t.tm_sec  = 0;
+    t.tm_min  = 0;
+    t.tm_hour = 0;
+    t.tm_mday = day_;
+    t.tm_mon  = month_ - 1;
+    t.tm_year = year_ - 1900;
+    t.tm_isdst = 0;
+    return mktime(&t);
+  }
+
+private:
+  int year_;
+  int month_;
+  int day_;
+
+  Date(int year, int month, int day)
+    : year_(year), month_(month), day_(day) {}
 };
 
-int ToInt(string s){
-	stringstream ss(s);
-	int r;
-	ss >> r;
-	return r;
+int ComputeDaysDiff(const Date& date_to, const Date& date_from) {
+  const time_t timestamp_to = date_to.AsTimestamp();
+  const time_t timestamp_from = date_from.AsTimestamp();
+  static constexpr int SECONDS_IN_DAY = 60 * 60 * 24;
+  return (timestamp_to - timestamp_from) / SECONDS_IN_DAY;
 }
 
+static const Date START_DATE = Date::FromString("2000-01-01");
+static const Date END_DATE = Date::FromString("2100-01-01");
+static const size_t DAY_COUNT = ComputeDaysDiff(END_DATE, START_DATE);
 
+int main() {
+  cout.precision(25);
+  vector<double> money(DAY_COUNT, .0);
+  size_t num_queries;
+  cin >> num_queries;
 
-istream& operator>> (istream& in, Date& date);
-bool operator<= (const Date& dl, const Date& dr);
-bool operator< (const Date& dl, const Date& dr);
-int operator- (const Date& d1, const Date& d2);
-void operator+= (Date& d, int a);
+  for (size_t i = 0; i < num_queries; ++i) {
+    string command, date_from, date_to;
+    cin >> command >> date_from >> date_to;
+    Date from = Date::FromString(date_from);
+    Date to = Date::FromString(date_to);
+    auto start_indx = ComputeDaysDiff(from, START_DATE);
+    auto finish_indx = ComputeDaysDiff(to, START_DATE);
+    if (command == "Earn") {
+      double value;
+      cin >> value;
+      value /= ComputeDaysDiff(to, from) + 1;
+      for (int j = start_indx; j <= finish_indx; ++j) {
+        money[j] += value;
+      }
+    } else {
+      cout << accumulate(money.begin() + start_indx, money.begin() + finish_indx + 1, 0.) << '\n';
+    }
+  }
 
-
-	
-
-istream& operator>> (istream& in, Date& date){
-	string ex;
-	in >> ex;
-	stringstream ss(ex);
-	string tmp;
-	getline(ss, tmp, '-');
-	date.year = ToInt(tmp);
-	getline(ss, tmp, '-');
-	date.month = ToInt(tmp);
-	getline(ss, tmp, '-');
-	date.day = ToInt(tmp);
-	return in;
-}
-
-ostream& operator<< (ostream& out, const Date& date){
-	out << date.year << "-";
-	out << date.month << "-";
-	out << date.day << endl;
-	return out;
-}
-
-bool operator<= (const Date& dl, const Date& dr){
-	if(dl.year != dr.year){
-		return dl.year <= dr.year;
-	} else if (dl.month != dr.month){
-		return dl.month <= dr.month;
-	} else {
-		return dl.day <= dr.day;
-	}
-}
-
-bool operator< (const Date& dl, const Date& dr){
-	if(dl.year != dr.year){
-		return dl.year < dr.year;
-	} else if (dl.month != dr.month){
-		return dl.month < dr.month;
-	} else {
-		return dl.day < dr.day;
-	}
-}
-
-int operator- (const Date& d1, const Date& d2){
-	Date dl = d1;
-	Date dr = d2;
-	int k = 0;
-	if(dl < dr){
-		k = -1;
-		while(dl < dr){
-			k--;
-			dl+=1;
-		}
-	} else {
-		k = 1;
-		while(dr < dl){
-			k++;
-			dr+=1;
-		}
-	}
-	return k;
-
-}
-
-void PrintMap(map<Date, int>& a){
-	cout << " map\n";
-	for(const auto& [k, x] : a){
-		// cout << " " << x;
-		cout << k << " = " << x << endl;
-	}
-	cout << endl;
-}
-
-
-void operator+= (Date& d, int a){
-	d.day += a;
-	if(d.month == 2){
-		if(d.year % 4 == 0){
-			if(d.day > 29){
-				d.day = 1;
-				d.month++;
-			}
-			return;
-		} else {
-			if(d.day > 28){
-				d.day = 1;
-				d.month++;
-			}
-			return;
-		}
-	}
-	if(d.day < 31){
-		return;
-	} else if (d.day > 31){
-		d.day = 1;
-		if(++d.month > 12){
-			d.month = 1;
-			d.year++;
-		}
-		return;
-	} else {
-		switch(d.month){
-			case 4:
-			case 6:
-			case 9:
-			case 11: {
-				d.day = 1;
-				d.month++;
-			}
-		}
-		return;
-	}
-
-}
-
-
-int main(){
-	map<Date, int> base;
-	int q;
-	cin >> q;
-	while(q-->0){
-		string command;
-		cin >> command;
-		Date date_from, date_to;
-		cin >> date_from >> date_to;
-		if(command == "ComputeIncome"){
-			int s = 0;
-			for(Date d = date_from; d <= date_to; d+=1){
-				if(base.count(d)){
-					s += base.at(d);
-				}
-			}
-			cout << s << endl;
-			// PrintMap(base);
-			// cout << accumulate(begin(base), end(base), s);
-		}else if(command == "Earn"){
-			int value;
-			cin >> value;
-			value /= date_to - date_from;
-			// cout << value << endl;
-			for(Date d = date_from; d <= date_to; d+=1){
-				if(base.count(d)){
-					base[d] += value;
-				}else{
-					base[d] = value;
-				}
-			}
-			// PrintMap(base);
-
-		}
-	}
-	return 0;
+  return 0;
 }
