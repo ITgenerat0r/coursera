fd743a846c6d18d1fbfde39af57b8fb1d0d4cf74
WorkMachine <ydalite02@gmail.com>
2021-08-03 19:57:47 +0400

    v8.3

--- Yellow/final/git_push.bat
+++ Yellow/final/git_push.bat
@@ -7,8 +7,8 @@ git add Yellow/final
 
 
 
-set /A d = %DATE:~0,2%
-set /A v = %DATE:~3,1%
+@set /A d = %DATE:~0,2%
+@set /A v = %DATE:~3,1%
 
 if %v% == 0 (git commit -m "v%DATE:~4,1%.%d%") else (git commit -m "v%DATE:~3,2%.%d%")
 
